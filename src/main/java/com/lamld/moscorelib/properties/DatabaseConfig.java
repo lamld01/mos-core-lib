8f19de68d71e97bd1bfef61dfeb798861e79fc5b
lam <lambro2510@gmail.com>
2025-02-17 23:39:40 +0700

    update core lib property config

---
+++ src/main/java/com/lamld/moscorelib/properties/DatabaseConfig.java
@@ -0,0 +1,16 @@
+package com.lamld.moscorelib.properties;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Setter
+@Getter
+@Configuration
+@ConfigurationProperties(prefix = "spring.datasource")
+public class DatabaseConfig {
+    private String url;
+    private String username;
+    private String password;
+}
