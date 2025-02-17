ef29e1154a30cb9be7a202723c08202467cba38c
lam <lambro2510@gmail.com>
2025-02-17 22:47:11 +0700

    update core lib remove main

--- src/main/java/com/lamld/moscorelib/MosCoreLibApplication.java
+++ src/main/java/com/lamld/moscorelib/MosCoreLibApplication.java
@@ -1,14 +1,13 @@
 package com.lamld.moscorelib;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+//@SpringBootApplication
 public class MosCoreLibApplication {
 
-  public static void main(String[] args) {
-    SpringApplication.run(MosCoreLibApplication.class, args);
-  }
+//  public static void main(String[] args) {
+//    SpringApplication.run(MosCoreLibApplication.class, args);
+//  }
 
 }
