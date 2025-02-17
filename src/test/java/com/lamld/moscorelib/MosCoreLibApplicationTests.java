152702417c4c4847ed83eed61033e07c67337692
lam <lambro2510@gmail.com>
2025-02-17 22:43:22 +0700

    update pom build

--- src/test/java/com/lamld/moscorelib/MosCoreLibApplicationTests.java
+++ src/test/java/com/lamld/moscorelib/MosCoreLibApplicationTests.java
@@ -1,9 +1,6 @@
 package com.lamld.moscorelib;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class MosCoreLibApplicationTests {
 
 }
