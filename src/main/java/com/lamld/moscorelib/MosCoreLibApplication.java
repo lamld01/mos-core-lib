b096a5665500d31f799e85392ec5aee4228c13ce
lam <lambro2510@gmail.com>
2025-02-17 19:39:01 +0700

    init

---
+++ src/main/java/com/lamld/moscorelib/MosCoreLibApplication.java
@@ -0,0 +1,13 @@
+package com.lamld.moscorelib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MosCoreLibApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MosCoreLibApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/lamld/moscorelib/MosCoreLibApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lamld.moscorelib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MosCoreLibApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
