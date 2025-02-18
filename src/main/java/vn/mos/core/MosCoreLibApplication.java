c22bf32f4fa85e1187223ca3126dbea8cd541752
lam <lambro2510@gmail.com>
2025-02-18 23:13:55 +0700

    update core lib

--- src/main/java/vn/mos/core/MosCoreLibApplication.java
+++ src/main/java/vn/mos/core/MosCoreLibApplication.java
@@ -3,7 +3,6 @@ package vn.mos.core;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
 public class MosCoreLibApplication {
 
   public static void main(String[] args) {
