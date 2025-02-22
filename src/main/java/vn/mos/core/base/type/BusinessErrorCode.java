5322f4e314710b441384151c1045200d2b3c96ff
lam <lambro2510@gmail.com>
2025-02-22 19:41:52 +0700

    update core lib v0.36

--- src/main/java/vn/mos/core/base/type/BusinessErrorCode.java
+++ src/main/java/vn/mos/core/base/type/BusinessErrorCode.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpStatus;
 @Getter
 public enum BusinessErrorCode {
 
+    // Common error code
     BAD_REQUEST("COMMON_400", "Bad request", HttpStatus.BAD_REQUEST),
     UNAUTHORIZED("COMMON_401", "Unauthorized", HttpStatus.UNAUTHORIZED),
     FORBIDDEN("COMMON_403", "Forbidden", HttpStatus.FORBIDDEN),
@@ -17,7 +18,15 @@ public enum BusinessErrorCode {
     CONFLICT("COMMON_409", "Conflict", HttpStatus.CONFLICT),
     UNSUPPORTED_MEDIA_TYPE("COMMON_415", "Unsupported media type", HttpStatus.UNSUPPORTED_MEDIA_TYPE),
     INTERNAL_SERVER_ERROR("COMMON_500", "Internal server error", HttpStatus.INTERNAL_SERVER_ERROR),
-    SERVICE_UNAVAILABLE("COMMON_503", "Service unavailable", HttpStatus.SERVICE_UNAVAILABLE);
+    SERVICE_UNAVAILABLE("COMMON_503", "Service unavailable", HttpStatus.SERVICE_UNAVAILABLE),
+
+    // User error code
+    USER_NOT_FOUND("USER_01", "User not found", HttpStatus.BAD_REQUEST),
+    USER_EXISTS("USER_02", "User already exists", HttpStatus.BAD_REQUEST),
+
+    // Wallet error code
+    WALLET_NOT_ENOUGH_BALANCE("WALLET_01", "Wallet not enough balance", HttpStatus.BAD_REQUEST),
+    ;
 
     private final String code;
     private final String message;
