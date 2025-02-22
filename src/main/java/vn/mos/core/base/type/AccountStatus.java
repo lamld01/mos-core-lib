ff15d90015ff59aaf21cffc030d831bbcbb1caa5
lam <lambro2510@gmail.com>
2025-02-22 19:27:30 +0700

    update core lib v0.34

---
+++ src/main/java/vn/mos/core/base/type/AccountStatus.java
@@ -0,0 +1,7 @@
+package vn.mos.core.base.type;
+
+public enum AccountStatus {
+  ACTIVATED,
+  INACTIVATED,
+  LOCKED
+}
---
+++ src/main/java/vn/mos/core/base/type/WalletStatus.java
@@ -0,0 +1,10 @@
+package vn.mos.core.base.type;
+
+public enum WalletStatus {
+  ACTIVATED,
+  DELETED,
+  EXPIRED,
+  LOCKED,
+  REMOVED,
+  UNLOCKED,
+}
